fc3b353a5f227d5cbb32183c39c4205a5da1b7c3
mngass2s <maurice.ngassa@smail.inf.h-brs.de>
2020-09-07 16:28:50 +0200

    x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import {BrowserAnimationsModule} from '@angular/platform browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule }   from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -12,8 +15,9 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule, MatButtonModule,
-    // BrowserAnimationsModule
+    BrowserModule, MatButtonModule, MatInputModule,
+    BrowserAnimationsModule,
+    FormsModule, MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
